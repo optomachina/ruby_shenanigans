6eefd5bbafef8fb248d6c343c5d050a5f0cbcc66
Blaine <130874646+optomachina@users.noreply.github.com>
2025-04-22 21:01:45 -0700

    did some shit

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,12 +1,17 @@
 class TestController < ApplicationController
   def hello
     @message = "Hello World"
-    @test_model = TestModel.all
+    @test_model = TestModel.new
+    @received_message = TestModel.all
+
   end
 
   def create
-    @test_model = TestModel.new(name: params[:name])
+    @test_model = TestModel.new(test_params)
     @test_model.save
   end
-end
 
+  def test_params
+    params.require(:test_model).permit(:name)
+  end
+end
